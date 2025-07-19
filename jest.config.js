86e96d2247ee3ca25607f638b34d6556041e35a1
Kripan <krpnspkt@gmail.com>
2025-07-19 15:25:11 +0545

    tested task but seems failed

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
-// jest.config.js
 const nextJest = require('next/jest');
 
-const createJestConfig = nextJest({ dir: './' });
+const createJestConfig = nextJest({
+  dir: './',
+});
 
 const customJestConfig = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/app/$1',
   },
